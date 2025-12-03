cb5ee744301719521b61273b03a4a15ca1646e5b
Banana <sarabialeo@gmail.com>
2025-12-02 17:57:09 -0800

    Added admin commands to easily gather the top posts in text channel. (Limited to posts with minimum 10 reacts to minimmize memory storage usage)

--- cogs/Miscellaneous.py
+++ cogs/Miscellaneous.py
@@ -3,6 +3,8 @@ from discord import app_commands
 from discord.ext import commands
 
 import asyncio
+import os
+from datetime import date, datetime, time
 
 from pathlib import Path
 import logging
@@ -35,6 +37,84 @@ class Miscellaneous(commands.Cog):
             await interaction.followup.send(f"{emoji}")
         else:
             await interaction.followup.send("Emoji not found.")
+
+    @app_commands.checks.has_role(int(os.getenv('MANAGER_ROLE')))  
+    @app_commands.command(name='top_10', description='Find the top 10 posts in this text channel.')
+    async def find_top_posts(self, interaction:discord.Interaction):
+        await interaction.response.defer()
+
+        if interaction.guild is None:
+            await interaction.followup.send("This command must be used within a text channel.", ephemeral=True)
+            return
+
+        channel:discord.TextChannel = interaction.channel
+        if not isinstance(channel, discord.TextChannel):
+            await interaction.followup.send("Text channel missing.", ephemeral=True)
+            return
+
+        # collect the messages
+        messages:list[tuple] = []
+        async for message in channel.history(limit=30000):
+            if not message.reactions:
+                continue
+
+            total_reactions = sum(reaction.count for reaction in message.reactions)
+            if total_reactions > 10:
+                messages.append((message,total_reactions))
+
+        # sort the messages
+        messages.sort(key=lambda x: x[1], reverse=True)
+        top_posts = messages[:25]
+
+        embed = discord.Embed(title = f'Top {len(top_posts)} Posts in {channel.name}', url='' ,description = '', color = self.emb_color ) 
+
+        for i, (mess,count) in enumerate(top_posts):
+            embed.add_field(name = f'#{i+1}', value = f'{count} Reactions - [Jump to Post]({mess.jump_url})', inline=False)
+
+        await interaction.followup.send(embed=embed)
+
+    @app_commands.checks.has_role(int(os.getenv('MANAGER_ROLE')))  
+    @app_commands.command(name='top_10_date', description='Find the top 10 posts in this text channel before a given date.')
+    async def find_top_posts(self, interaction:discord.Interaction, month:int, day:int, year:int):
+        await interaction.response.defer()
+
+        try:
+            before_date = date(year,month,day)
+            before_time = time(0,0,0)
+            before_datetime = datetime.combine(before_date,before_time)
+        except Exception as e:
+            await interaction.followup.send("Invalid date values provided.", ephemeral=True)
+            return
+
+        if interaction.guild is None:
+            await interaction.followup.send("This command must be used within a text channel.", ephemeral=True)
+            return
+
+        channel:discord.TextChannel = interaction.channel
+        if not isinstance(channel, discord.TextChannel):
+            await interaction.followup.send("Text channel missing.", ephemeral=True)
+            return
+
+        # collect the messages
+        messages:list[tuple] = []
+        async for message in channel.history(limit=20000,before=before_datetime):
+            if not message.reactions:
+                continue
+
+            total_reactions = sum(reaction.count for reaction in message.reactions)
+            if total_reactions > 10:
+                messages.append((message,total_reactions))
+
+        # sort the messages
+        messages.sort(key=lambda x: x[1], reverse=True)
+        top_posts = messages[:25]
+
+        embed = discord.Embed(title = f'Top {len(top_posts)} Posts in {channel.name} before {before_datetime.date}', url='' ,description = '', color = self.emb_color ) 
+
+        for i, (mess,count) in enumerate(top_posts):
+            embed.add_field(name = f'#{i+1}', value = f'{count} Reactions - [Jump to Post]({mess.jump_url})', inline=False)
+
+        await interaction.followup.send(embed=embed)
 
 
     ###################################################
